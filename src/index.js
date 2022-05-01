4566d454b9e91ac007b80e74eef5fc635cbb57a2
mcerqs <m.cerqueira@live.com>
2022-05-01 09:23:33 -0300

    First queries, resolvers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log('dsads');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      user: User!
+      users: [User!]!
+    }
+
+    type User {
+      id: ID!
+      name: String!
+      username: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      user: () => {
+        return {
+          id: 'asdf-4312',
+          name: 'Isabela Cerqueira',
+          username: 'isalinda',
+        };
+      },
+      users: () => {
+        return [
+          {
+            id: 'asdf-4312',
+            name: 'Isabela Cerqueira',
+            username: 'isalinda',
+          },
+          {
+            id: 'rtys-5678',
+            name: 'Marcos Cerqueira',
+            username: 'mcerqs',
+          },
+          {
+            id: 'daqw-0934',
+            name: 'Isla Cerqueira',
+          },
+          {
+            id: 'oier-1290',
+            name: 'Noah Cerqueira',
+          },
+        ];
+      },
+    },
+  },
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});
